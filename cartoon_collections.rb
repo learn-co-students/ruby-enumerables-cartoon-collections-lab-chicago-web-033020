340066c1db186ff8d8b194c27950a07cf64f65d7
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-23 09:39:37 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#https://www.rubyguides.com/2018/10/any-all-none-one/
+def roll_call_dwarves(array)
+  array.each_with_index { |item, index|
+    puts "#{(index+1)}. #{item.capitalize}\n"
+  }
+end
+############
+def summon_captain_planet(array)
+  array.map! { |calls| calls.capitalize + "!"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|item| item.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |cheese| cheese_types.include?(cheese)}
 end
